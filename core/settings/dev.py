097849932a4ec971a3a7cf2cdf268908db5de84f
mlavrynenko <mlavrynenko@n-ix.com>
2026-02-26 01:26:13 +0200

    missing constructor parameter

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -6,9 +6,7 @@ class DevSettings(BaseSettings):
     Development configuration.
     """
 
-    def __init__(self) -> None:
-        super().__init__()
-
-        # Dev allows missing RESULTS_BUCKET
+    def __init__(self, skip_secrets: bool = False) -> None:
+        super().__init__(skip_secrets=skip_secrets)
         self.DEBUG = True
         
